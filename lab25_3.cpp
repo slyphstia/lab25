8ba3d11f9c33806eb668671e4da3ef07ada08427
hghgh <apiwit_boonyarit@cmu.ac.th>
2023-03-18 13:14:33 +0700

    complete

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
 //Write function count() here.
+int count(int A[], int N){
+	string B;
+	set<string> myset;
+	for(int i = 0; i < N;i++){
+		B = A[i];
+		myset.insert(B);
+	}
+	return myset.size();
+}
+
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
